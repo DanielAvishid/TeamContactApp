cce3c2c72d6f13577f255c4062a0a626f97dc03b
Daniel Avishid <danielavishid@gmail.com>
2023-09-19 15:24:24 +0300

    F/add-contact-service/contact serviceadd and clean

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,20 +2,16 @@ const { Route, Routes } = ReactRouterDOM
 const Router = ReactRouterDOM.HashRouter
 const { Provider } = ReactRedux
 
-import { AppHeader } from './cmps/AppHeader.jsx'
 import { Home } from './views/Home.jsx'
 
 export function App() {
     return (
-        <Provider store={store}>
-            <Router>
-                <section className="app">
-                    <AppHeader />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                    </Routes>
-                </section>
-            </Router>
-        </Provider>
+        <Router>
+            <section className="app">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </section>
+        </Router>
     )
 }
---
+++ services/contact.service.js
@@ -0,0 +1,82 @@
+import { storageService } from './async-storage.service.js'
+import { utilService } from './util.service.js'
+
+const STORAGE_KEY = 'contactDB'
+
+_createContacts()
+
+export const contactService = {
+    query,
+    getById,
+    save,
+    remove,
+    getEmptyContact,
+}
+
+function query() {
+    return storageService.query(STORAGE_KEY)
+        .then(contacts => {
+            return contacts
+        })
+}
+
+function getById(contactId) {
+    return storageService.get(STORAGE_KEY, contactId)
+}
+
+function remove(contactId) {
+    return storageService.remove(STORAGE_KEY, contactId)
+}
+
+function save(contact) {
+    if (contact._id) {
+        return storageService.put(STORAGE_KEY, contact)
+    } else {
+        return storageService.post(STORAGE_KEY, contact)
+    }
+}
+
+function getEmptyContact() {
+    return {}
+}
+
+function _createContacts() {
+    let contacts = utilService.loadFromStorage(STORAGE_KEY)
+    if (!contacts || !contacts.length) {
+        contacts = [
+            {
+                id: utilService.makeId(),
+                firstname: 'Daniel',
+                lastname: 'Avishid',
+                email: 'danielavishid@gmail.com',
+                phone: '052-7457722',
+                desc: 'description txt'
+            },
+            {
+                id: utilService.makeId(),
+                firstname: 'Ofir',
+                lastname: 'Kaspi',
+                email: 'ofirkaspi@gmail.com',
+                phone: '052-3213151',
+                desc: 'description txt'
+            },
+            {
+                id: utilService.makeId(),
+                firstname: 'Sapir',
+                lastname: 'Avishid',
+                email: 'sapiravishid@gmail.com',
+                phone: '052-7457332',
+                desc: 'description txt'
+            },
+            {
+                id: utilService.makeId(),
+                firstname: 'Ari',
+                lastname: 'Avishid',
+                email: 'ariavishid@gmail.com',
+                phone: '052-7457333',
+                desc: 'description txt'
+            }
+        ]
+        utilService.saveToStorage(STORAGE_KEY, contacts)
+    }
+}
